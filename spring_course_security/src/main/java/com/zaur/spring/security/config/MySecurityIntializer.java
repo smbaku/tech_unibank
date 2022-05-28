c5ab546a1062da16a207d269e823db000a96f909
Saleh Mirzeyev <51265563+smbaku@users.noreply.github.com>
2022-05-28 11:48:20 +0400

    Delete spring_course_security directory

--- spring_course_security/src/main/java/com/zaur/spring/security/config/MySecurityIntializer.java
+++
@@ -1,6 +0,0 @@
-package com.zaur.spring.security.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class MySecurityIntializer extends AbstractSecurityWebApplicationInitializer {
-}
